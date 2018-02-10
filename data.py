d64cc830d50d36ca66562e9937b02be3ba403358
Cyrus Sethna <csethna@gmail.com>
2018-02-10 13:38:57 -0500

    data quality

--- data.py
+++ data.py
@@ -1,9 +1,9 @@
 import os
 import pandas as pd
-import tabulate
+import tabulate as tb
 
 CSV_PATH = os.path.join('dctechnology.csv')
-CSV_PATH = CSV_PATH.encode('utf-8').strip()
+# CSV_PATH = CSV_PATH.encode('utf-8').strip()
 
 df = pd.read_csv(CSV_PATH)
 
