c15b3c236f0ff07622de967476dc1fa1da2d0da2
Cyrus Sethna <csethna@gmail.com>
2018-02-10 13:43:32 -0500

    ParserError Fix

--- data.py
+++ data.py
@@ -5,6 +5,8 @@ import tabulate as tb
 CSV_PATH = os.path.join('dctechnology.csv')
 # CSV_PATH = CSV_PATH.encode('utf-8').strip()
 
-df = pd.read_csv(CSV_PATH)
+cols = ["first", "last", "outlet", "title", "contact"]
+
+df = pd.read_csv(CSV_PATH, names=cols, engine='python')
 
 print(df)
