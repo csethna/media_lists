f78db585f72da21f0b847901bb91283267c55a15
Cyrus Sethna <csethna@gmail.com>
2018-02-10 10:03:25 -0500

    importing csv

--- data.py
+++ data.py
@@ -1,6 +1,10 @@
 import os
-import pandas as import pd
+import pandas as pd
+import tabulate
 
 CSV_PATH = os.path.join('dctechnology.csv')
+CSV_PATH = CSV_PATH.encode('utf-8').strip()
 
 df = pd.read_csv(CSV_PATH)
+
+print(df)
